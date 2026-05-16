c87bd4afc2d18b3a15ed67582ba55bf26ec20ef4
IT25101884 <it25101884@my.sliit.lk>
2026-05-16 10:28:52 +0530

    Update ReviewNotFoundException

--- backend/src/main/java/com/photobooking/review/exception/ReviewNotFoundException.java
+++ backend/src/main/java/com/photobooking/review/exception/ReviewNotFoundException.java
@@ -1,7 +1,7 @@
 package com.photobooking.review.exception;
 
 public class ReviewNotFoundException extends RuntimeException {
-    public ReviewNotFoundException(String message) {
-        super(message);
+   public ReviewNotFoundException(Long id) {
+        super("Review not found with id: " + id);
     }
 }
