577dde050e01cebdcd203ea79860584759b25a39
it25100557 <it25100557@my.sliit.lk>
2026-05-19 11:57:56 +0530

    Update MediaStaffNotFoundException

--- backend/src/main/java/com/photobooking/mediastaff/exception/MediaStaffNotFoundException.java
+++ backend/src/main/java/com/photobooking/mediastaff/exception/MediaStaffNotFoundException.java
@@ -1,4 +1,8 @@
 package com.photobooking.mediastaff.exception;
 
 public class MediaStaffNotFoundException extends RuntimeException {
+
+    public MediaStaffNotFoundException(Long id) {
+        super("Media staff not found with id: " + id);
+    }
 }
