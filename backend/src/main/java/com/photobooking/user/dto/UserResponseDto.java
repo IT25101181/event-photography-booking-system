e61e02745eaf02ef7a042f7f7b3d33dd9bb2d697
it25101430 <it25101430@my.sliit.lk>
2026-05-19 13:24:05 +0530

    Update UserResponseDto

--- backend/src/main/java/com/photobooking/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/photobooking/user/dto/UserResponseDto.java
@@ -1,4 +1,26 @@
 package com.photobooking.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserResponseDto {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String role;
+    
+    @JsonProperty("isActive")
+    private boolean isActive;
+    
+    private LocalDateTime createdAt;
 }
