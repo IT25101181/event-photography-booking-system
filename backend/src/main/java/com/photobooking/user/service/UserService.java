267d1af03e68f4f145d9ad45a7b4afe85ec84f5d
it25101430 <it25101430@my.sliit.lk>
2026-05-19 13:25:27 +0530

    Update UserService

--- backend/src/main/java/com/photobooking/user/service/UserService.java
+++ backend/src/main/java/com/photobooking/user/service/UserService.java
@@ -1,4 +1,16 @@
 package com.photobooking.user.service;
 
+import com.photobooking.user.dto.UserRequestDto;
+import com.photobooking.user.dto.UserResponseDto;
+
+import java.util.List;
+
 public interface UserService {
+    UserResponseDto registerUser(UserRequestDto dto);
+    UserResponseDto getUserById(Long id);
+    List<UserResponseDto> getAllUsers();
+    UserResponseDto updateUser(Long id, UserRequestDto dto);
+    void deleteUser(Long id);
+    void toggleUserStatus(Long id);
+    UserResponseDto getUserByEmail(String email);
 }
